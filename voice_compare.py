e501fd7392829f5d87cc2f389d99fed874320313
Unnati13 <unnatiagarwal1313@gmail.com>
2026-03-08 23:18:42 +0530

    Normalized audio and imporved threshold

--- voice_compare.py
+++ voice_compare.py
@@ -1,5 +1,10 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
+import librosa
+import numpy as np
+from dtaidistance import dtw
+from scipy.spatial.distance import euclidean
+
 
 def record_audio(filename, duration=3, fs=44100):
     print("Recording...")
@@ -10,11 +15,15 @@ def record_audio(filename, duration=3, fs=44100):
     write(filename, fs, audio)
     print("Saved:", filename)
 
-import librosa
-import numpy as np
 
 def extract_features(file):
-    audio, sr = librosa.load(file)
+    audio, sr = librosa.load(file, sr=16000)
+
+    # ✅ normalize volume
+    audio = librosa.util.normalize(audio)
+
+    # ✅ REMOVE SILENCE
+    audio, _ = librosa.effects.trim(audio, top_db=20)
 
     mfcc = librosa.feature.mfcc(
         y=audio,
@@ -22,16 +31,27 @@ def extract_features(file):
         n_mfcc=13
     )
 
-    return np.mean(mfcc.T, axis=0)
+    return mfcc.T
 
 
 
+def similarity(mfcc1, mfcc2):
 
-from numpy.linalg import norm
+    distances = []
 
-def similarity(v1, v2):
-    return np.dot(v1, v2) / (norm(v1) * norm(v2))
+    for i in range(mfcc1.shape[1]):
+        d = dtw.distance(
+            mfcc1[:, i],
+            mfcc2[:, i]
+        )
+        distances.append(d)
 
+    final_distance = np.mean(distances)
+
+    # ✅ normalize by length
+    final_distance /= (len(mfcc1) + len(mfcc2))
+
+    return final_distance
 
 record_audio("voice1.wav")
 input("Press Enter for second recording...")
@@ -45,7 +65,7 @@ score = similarity(f1, f2)
 
 print("Similarity Score:", score)
 
-if score > 0.8:
+if score < 1.8:
     print("✅ Same Speaker")
 else:
     print("❌ Different Speaker")
